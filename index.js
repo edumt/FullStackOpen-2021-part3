0b7a7a7f453d6973be7a61cfd4f7b4fd18047a83
Eduardo Moura Tavares <edumoura1997@gmail.com>
2021-12-14 20:52:45 -0300

    3.8

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const morgan = require("morgan");
 const express = require("express");
 const app = express();
-
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token("sent-data", function (req, res) {
+  if (req.method === "POST") return JSON.stringify(req.body);
+});
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :sent-data"
+  )
+);
 
 let persons = [
   {
