ddd39a349f11fb4a60bf704c28f5f1c9364b1ec3
Eduardo Moura Tavares <edumoura1997@gmail.com>
2021-12-16 20:25:26 -0300

    setting up heroku app

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const morgan = require("morgan");
 const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 morgan.token("sent-data", function (req, res) {
   if (req.method === "POST") return JSON.stringify(req.body);
@@ -81,7 +83,7 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
