b19b57d137492f79b6ea7d821cb2f7d65875e648
Eduardo Moura Tavares <edumoura1997@gmail.com>
2021-12-16 20:46:57 -0300

    added frontend production build

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const morgan = require("morgan");
 const express = require("express");
 const cors = require("cors");
 const app = express();
+app.use(express.static("build"));
 app.use(express.json());
 app.use(cors());
 
