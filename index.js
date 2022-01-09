5e1e9c2123e9937d24c27272a4439653bc01a041
Eduardo Moura Tavares <edumoura1997@gmail.com>
2022-01-09 19:17:29 -0300

    3.17

--- index.js
+++ index.js
@@ -68,13 +68,14 @@ app.get("/api/persons/:id", (request, response, next) => {
 });
 
 app.post("/api/persons", (request, response) => {
-  if (!(request.body.name && request.body.number)) {
+  const body = request.body;
+  if (!(body.name && body.number)) {
     return response
       .status(400)
       .send({ error: "The name or number is missing." });
   }
 
-  const person = new Person({ ...request.body });
+  const person = new Person({ name: body.name, number: body.number });
   /* if (persons.some((_person) => _person.name === person.name)) {
     return response
       .status(409)
@@ -84,6 +85,17 @@ app.post("/api/persons", (request, response) => {
   person.save().then((savedPerson) => {
     response.json(savedPerson);
   });
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+  const person = { name: body.name, number: body.number };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response, next) => {
